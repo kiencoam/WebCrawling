8495f0884980bbe51dabdf3e1683b4cd7d4e5f48
Kien Pham <kienpham1392004@gmail.com>
2024-05-21 22:22:13 +0700

    update to uml diagram

--- src/main/java/webCrawling/WebCrawler.java
+++ src/main/java/webCrawling/WebCrawler.java
@@ -1,10 +1,16 @@
 package webCrawling;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,6 +20,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.jsoup.Jsoup;
@@ -36,7 +43,7 @@ public class WebCrawler {
 	 * Các bài viết được lấy về là các bài viết được đăng sau thời gian cập nhật gần nhất 
 	 */
 	public void crawl() throws IOException, URISyntaxException, ParseException {
-		Controller.postResource(web);
+		postResource(web);
 		
 		LocalDate lastestUpdateTime = web.retriveLastestUpdateTime();
 		System.out.println(lastestUpdateTime);
@@ -62,7 +69,7 @@ public class WebCrawler {
 					if(article.getHashtags() == null) generateHashtags(article);
 					if(article.isValid()) {
 						store(article);
-						Controller.postArticle(article);
+						postArticle(article);
 					}
 				} else break breakLabel;
 			}
@@ -139,6 +146,48 @@ public class WebCrawler {
 		FileWriter writer = new FileWriter(storageAddress);
 		writer.write(jsonArticles.toJSONString());
 		writer.close();
+	}
+	
+	private void postResource(Website web) throws IOException, URISyntaxException {
+		JSONObject jsonObject = web.convertToJSONObject();		
+        postMethod(jsonObject.toJSONString(), "resources");
+	}
+	
+	private void postArticle(Article article) throws URISyntaxException, IOException {
+        JSONObject jsonArticle = article.convertToJSONObject();
+        postMethod(jsonArticle.toJSONString(), "articles/create");
+	}
+	
+	private void postMethod(String s, String pathSegment) throws IOException, URISyntaxException {
+		String urlString = "http://localhost:5454/api/" + pathSegment;
+		URI uri = new URI(urlString);
+		URL url = uri.toURL();
+		
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("POST");
+		connection.setRequestProperty("Content-Type", "application/json");
+		connection.setDoOutput(true);
+		connection.setDoInput(true);
+        
+		try {
+			try(OutputStream os = connection.getOutputStream()) {
+			    byte[] input = s.getBytes("utf-8");
+			    os.write(input, 0, input.length);			
+			}
+	        
+	        // Nhận và in ra thông tin phản hồi từ phía local host        
+	        try(BufferedReader br = new BufferedReader(
+					new InputStreamReader(connection.getInputStream(), "utf-8"))) {
+					StringBuilder response = new StringBuilder();
+					String responseLine = null;
+					while ((responseLine = br.readLine()) != null) {
+					response.append(responseLine.trim());
+			    }
+			    System.out.println(response.toString());
+			}
+		} catch (Exception e){
+			System.err.println("Error sending JSON: " + e.getMessage());
+		}
 	}
 	
 }
